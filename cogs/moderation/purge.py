f8b0654a6c8dc617085c637985c397ab00680e3b
Jaimss <james@harrell365.com>
2020-08-28 10:50:49 -0400

    purge check

--- cogs/moderation/purge.py
+++ cogs/moderation/purge.py
@@ -1,8 +1,8 @@
 from discord.ext import commands as com
-
-from util.util import (quick_response, error_response)
+from discord import DMChannel
 
 from util.checks import is_pinned
+from util.util import (quick_response, error_response, warn_response)
 
 
 class Purge(com.Cog):
@@ -15,6 +15,21 @@ class Purge(com.Cog):
     async def purge_command(self, ctx, limit=None):
         if limit is None:
             limit = "50000"
+
+        def check_rxn(reaction, user):
+            if user != ctx.message.author:
+                return False
+            if str(reaction.emoji) == "❌" or "✅":
+                return True
+            else:
+                return False
+
+        if limit == "50000":
+            await warn_response(ctx.channel, 'You are about to clear the channel!\nReact ✅ to continue or ❌ to cancel!')
+            reaction, user = await self.bot.wait_fot('reaction_add', check=check_rxn)
+            if str(reaction.emoji) == "❌":
+                await quick_response(ctx.channel, 'Canceled Purge!')
+                return
 
         if limit.isdigit():
             messages = await ctx.channel.purge(limit=int(limit), check=is_pinned)
