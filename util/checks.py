c72a544e60a8ad25c1de3a762b3c73e15cbc0314
Jaimss <james@harrell365.com>
2020-08-31 22:17:24 -0400

    Update checks.py

--- util/checks.py
+++ util/checks.py
@@ -2,5 +2,5 @@ async def is_owner(ctx):
     return ctx.message.author.id == 348146715162968065
 
 
-async def is_pinned(message) -> bool:
-    return message.pinned
+def is_pinned(message) -> bool:
+    return not message.pinned
